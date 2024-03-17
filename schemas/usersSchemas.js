ad3ae39b62690a0f2130664ba572ce30d8dda861
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-17 12:45:28 +0200

    fix user-register-schema

--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -2,7 +2,6 @@ import Joi from "joi";
 import { emailRegexp } from "../constants/regexp.js";
 
 export const singupSchema = Joi.object({
-  name: Joi.string().min(2),
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(8).required(),
 });
