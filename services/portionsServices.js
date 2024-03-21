e54327901d37b852721535f701e97325389889bc
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-21 17:44:18 +0200

    added portionService for month

--- services/portionsServices.js
+++ services/portionsServices.js
@@ -12,29 +12,27 @@ export const updatePortion = async (id, body) =>
   Portion.findByIdAndUpdate(id, body);
 
 export const findPortionsByMonthAndUser = async (userId, month) => {
-  const startOfMonth = new Date(Date.UTC(new Date().getFullYear(), month - 1, 1));
-  const endOfMonth = new Date(Date.UTC(new Date().getFullYear(), month, 0, 23, 59, 59, 999));
-
-  // Запит до бази даних для отримання всіх порцій за вказаний місяць
-  const portions = await Portion.find({
-    createdAt: {
-      $gte: startOfMonth,
-      $lt: endOfMonth,
-    },
-  });
-
-  // Фільтруємо порції лише для вказаного користувача
-  const userPortions = portions.filter(portion => portion.userId === userId);
-
-  return userPortions;
+  // const startOfMonth = new Date(Date.UTC(new Date().getFullYear(), month - 1, 1));
+  // const endOfMonth = new Date(Date.UTC(new Date().getFullYear(), month, 0, 23, 59, 59, 999));
 
   // const portions = await Portion.find({
-  //   userId: userId,
-  //   $expr: {
-  //     $and: [{ $eq: [{ $month: "$createdAt" }, month] }],
+  //   createdAt: {
+  //     $gte: startOfMonth,
+  //     $lt: endOfMonth,
   //   },
   // });
-  // return portions;
+
+  // const userPortions = portions.filter(portion => portion.userId === userId);
+
+  // return userPortions;
+
+  const portions = await Portion.find({
+    userId: userId,
+    $expr: {
+      $and: [{ $eq: [{ $month: "$createdAt" }, month] }],
+    },
+  });
+  return portions;
 };
 
 export const findPortionsByDayAndUser = async (userId, startDate, endDate) => {
