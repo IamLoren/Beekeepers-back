c7b8791c011c3f7739701fbc54aaae6f8ea886f8
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-21 09:47:18 +0200

    install cors and add app.use.cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRouter.js";
@@ -13,7 +13,12 @@ dotenv.config();
 
 const app = express();
 
-
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+//   methods: ["GET, POST, PUT, DELETE"]
+// }));
+app.use(cors()); 
 app.use(morgan("tiny"));
 app.use(express.json());
 
