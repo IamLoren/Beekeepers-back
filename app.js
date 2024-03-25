cee5236d943e962a45276a5c8cf694cf7f5813db
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-25 14:39:51 +0200

    comment swagger

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocument from './swagger.json' assert { type: 'json' };
+// import swaggerUi from "swagger-ui-express";
+// import swaggerDocument from './swagger.json' assert { type: 'json' };
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRouter);
 app.use("/api/portions", portionsRouter);
 
